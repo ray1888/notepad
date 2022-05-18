3c3cba4f2f66931b8d6cf211194aae66ae7b8936
ray1888 <alal1995@hotmail.com>
2022-05-18 15:42:46 +0800

    feat: add npm build

--- react-vite-h5/src/App.jsx
+++ react-vite-h5/src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import {
   BrowserRouter as Router,
-  Switch,
+  Routes,
   Route
 } from "react-router-dom"
 
@@ -13,13 +13,13 @@ import routes from '../src/router'
 function App() {
   return <Router>
     <ConfigProvider primaryColor={'#007fff'} locale={zhCN}>
-      <Switch>
+      <Route>
         {
           routes.map(route => <Route exact key={route.path} path={route.path}>
             <route.component />
           </Route>)
         }
-      </Switch>
+      </Route>
     </ConfigProvider>
   </Router>
 }
