f049a0076a043a8e168a7cbb1975a3a79a2f784e
ray1888 <alal1995@hotmail.com>
2022-05-18 15:31:04 +0800

    feat: add zarm

--- react-vite-h5/src/App.jsx
+++ react-vite-h5/src/App.jsx
@@ -4,16 +4,23 @@ import {
   Switch,
   Route
 } from "react-router-dom"
+
+import { ConfigProvider } from 'zarm'
+import zhCN from 'zarm/lib/config-provider/locale/zh_CN'
+import 'zarm/dist/zarm.css'
+
 import routes from '../src/router'
 function App() {
   return <Router>
-    <Switch>
-      {
-        routes.map(route => <Route exact key={route.path} path={route.path}>
-          <route.component />
-        </Route>)
-      }
-    </Switch>
+    <ConfigProvider primaryColor={'#007fff'} locale={zhCN}>
+      <Switch>
+        {
+          routes.map(route => <Route exact key={route.path} path={route.path}>
+            <route.component />
+          </Route>)
+        }
+      </Switch>
+    </ConfigProvider>
   </Router>
 }
 
